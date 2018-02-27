637314852d6d7f2b86d2e8d3d2e7a872910db98b
Kruhm <nicolaige@protonmail.com>
2018-02-27 19:10:58 +0100

    added some comments

--- app.cpp
+++ app.cpp
@@ -7,7 +7,6 @@ int square(int x){
 App::App(QWidget *parent): QMainWindow(parent) {
     this->hlayout = new QHBoxLayout(this);
     this->bgrp = new QButtonGroup(this);
-
     this->minesweeper = new Minesweeper(9);
 
     setupUI();
@@ -18,16 +17,22 @@ App::~App() {
 }
 
 void App::print() {
+    /*
+     * show the intern game field
+     * in console
+     */
     this->minesweeper->printField();
 }
 
 void App::setupUI(){
+    /*
+     * Initialize and build the GUI
+     */
     //add Buttons
     int bs = square(minesweeper->getBoardSize());
     for(int i = 0; i < bs; i++){
         QPushButton* btn = new QPushButton;
         hlayout->addWidget(btn);
-        //btn->show();
         bgrp->addButton(btn);
     }
 
